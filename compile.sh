5f1d5026eb420209ee7523d0785a3595d97259da
ZhongYic <zhong200234@outlook.com>
2019-03-03 23:51:16 +0800

    fixed a bug

--- compile.sh
+++ compile.sh
@@ -3,13 +3,13 @@ fullname=$GEDIT_CURRENT_DOCUMENT_NAME
 dir=$GEDIT_CURRENT_DOCUMENT_DIR
 
 name=`echo $fullname | cut -d. -f1`  
-g++ $fullname -o $name.run -Wall
+g++ $fullname -pthread -o $name.run -Wall
 
 if [ $? -ne 0 ]; then
     exit;
 fi
 
 deepin-terminal --working-directory=$dir -x bash -c \
-"$dir/$name.run;echo;echo;time" \
+"'$dir/$name.run';echo;echo;time" \
 >/dev/null  2>&1
 
